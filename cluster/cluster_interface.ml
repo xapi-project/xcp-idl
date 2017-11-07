0049e612f24465ea413b86e275f8684b0cd38d05
Jonathan Davies <jonathan.davies@citrix.com>
2017-11-07 15:36:47 +0000

    cluster: diagnostics: support next_cluster_config field
    Signed-off-by: Jonathan Davies <jonathan.davies@citrix.com>

--- cluster/cluster_interface.ml
+++ cluster/cluster_interface.ml
@@ -53,6 +53,7 @@ type cluster_config_and_all_members = cluster_config * all_members [@@deriving r
 
 type diagnostics = {
   live_cluster_config : cluster_config option; (* live corosync config *)
+  next_cluster_config : cluster_config option; (* next corosync config *)
   saved_cluster_config : cluster_config option; (* xapi-clusterd DB *)
   is_enabled : bool;
   all_members : all_members option;

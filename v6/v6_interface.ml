5376b10e97a34a72d516ab419552c74d0f8e51b0
Akanksha Mathur <akanksha.mathur@citrix.com>
2017-12-05 12:00:00 +0000

    CP-26098: ocp-indent and refactor errors and records
    includes the following:
    - refactor error handling
    - rename record fields and functor to prevent namespace clashes
    - edit docstrings
    
    Note: Types require a docstring above, not below
    
    ocp-indent
    
    [v6d] Define error handling, rename record fields and functor to avoid namespace
    clashes
    
    Edit v6 interface docstrings
    
    Signed-off-by: Akanksha Mathur <akanksha.mathur@citrix.com>
    
    CP-26098: Wrap matcher and raiser in parentheses
    
    Signed-off-by: Akanksha Mathur <akanksha.mathur@citrix.com>

--- v6/v6_interface.ml
+++ v6/v6_interface.ml
@@ -12,6 +12,9 @@
  * GNU Lesser General Public License for more details.
  *)
 
+(**
+   @group Licensing
+*)
 open Rpc
 open Idl
 
@@ -22,54 +25,56 @@ let default_path = ref (Filename.concat default_sockets_dir service_name)
 let uri () = "file:" ^ !default_path
 let json_path = "/var/xapi/v6.json"
 let xml_path = "/var/xapi/v6"
+
 (* --- global types used in most API calls --- *)
 
-type debug_info = string
 (** Uninterpreted/sanitised string associated with operation *)
+type debug_info = string
 [@@deriving rpcty]
 
+(** Record representing software edition *)
 type edition =
-{
-  name : string ;
-  (** Name of edition, this will be passed to apply_edition *)
-  official_title : string ;
-  (** Marketing title used to advertise edition *)
-  code : string ;
-  (** Abbreviated form of name, used to show up in logs and on command line *)
-  order : int ;
-  (** Number indicating ordering among other editions
-      with low numbers corresponding to editions with fewer features, and vice versa *)
-}
+  {
+    ed_title : string ;
+    (** Name of edition, this will be passed to apply_edition *)
+    ed_official_title : string ;
+    (** Marketing title used to advertise edition *)
+    ed_code : string ;
+    (** Abbreviated form of name, used to show up in logs and on command line *)
+    ed_order : int ;
+    (** Number indicating ordering among other editions
+        with low numbers corresponding to editions with fewer features, and vice versa *)
+  }
 [@@deriving rpcty]
 
-type edition_list = edition list
 (** List of edition records *)
+type edition_list = edition list
 [@@deriving rpcty]
 
+(** Record of edition info, including xapi parameters and features *)
 type edition_info =
-{
-  edition: string;
-	(** Name of edition *)
-  xapi_params: (string * string) list;
-	(** List of parameters used by Xapi *)
-  additional_params: (string * string) list;
-	(** Addition parameters supplied *)
-  experimental_features: (string * bool) list;
-	(** List of experimental features and whether they're available in this edition *)
-}
+  {
+    edition_name: string;
+    (** Name of edition *)
+    xapi_params: (string * string) list;
+    (** List of parameters used by Xapi *)
+    additional_params: (string * string) list;
+    (** Addition parameters supplied *)
+    experimental_features: (string * bool) list;
+    (** List of experimental features and whether they're available in this edition *)
+  }
 [@@deriving rpcty]
 
-type stringPairLst = (string * string) list
 (** [string * string] list *)
+type stringPairLst = (string * string) list
 [@@deriving rpcty]
 
 (* --- errors wrapped in generic errors type --- *)
 
+(** Wrapper for specific licensing errors *)
 type errors =
   | Invalid_edition of string
   (** Thrown by apply_edition on receiving unfamiliar edition. Note: get_editions returns list of all valid editions *)
-  | V6d_failure
-  (** Licensing daemon failed *)
   | License_expired
   (** Thrown by license_check when expiry date matches or precedes current date *)
   | License_processing_error
@@ -78,64 +83,68 @@ type errors =
   (** License could not be checked out *)
   | Missing_connection_details
   (** Thrown if connection port or address parameter not supplied to check_license *)
+  | V6d_failure
+  (** Licensing daemon failed *)
 [@@default V6d_failure]
 [@@deriving rpcty]
 (* --- API interface --- *)
 
+(** Generic licensing error of error variant type *)
 exception LicensingError of errors
-(** Generic error wrapper for specific licensing errors *)
 [@@deriving rpcty]
 
-module API(R : RPC) = struct
+(** handle exception generation and raising *)
+let err = Error.{
+    def = errors;
+    raiser = (function
+        | e -> raise (LicensingError e));
+    matcher = (function
+      | LicensingError e -> Some e
+      | _ -> None)
+  }
+
+(** functor to autogenerate code using PPX *)
+module RPC_API(R : RPC) = struct
   open R
 
-  (* description of functions *)
+  (* description of V6d interface module *)
   let description = Interface.{
       name = "Licensing";
       namespace = None;
       description = [
         "This interface is used by Xapi and V6d to manage ";
         "XenServer edition licensing of hosts.";
-        ];
+      ];
       version=(1,0,0);        }
 
   (* define implementation *)
   let implementation = implement description
 
-  (* define global parameters for API calls *)
+  (* define general parameters for API calls *)
   let debug_info_p = Param.mk ~description:[
-    "An uninterpreted string to associate with the operation."
-    ] debug_info 
+      "An uninterpreted string to associate with the operation."
+    ] debug_info
 
-  (* ---- TODO : include functor to give 'err' type : TODO ---- *)
+  (* ---- API call definitions ---- *)
 
-  module E  = Idl.Error.Make(struct type t = LicensingError let t=LicensingError end)
-
-  let err = Error.{
-    def = errors;
-    raiser = (function | e -> raise (LicensingError e));
-    matcher = function | LicensingError e -> Some e | _ -> None
-  }
-
-  (* dbg_str -> requested edition -> current params -> edition_info *)
   let apply_edition =
-    let edition_p = Param.mk ~description:["Edition tite"] Types.string in
-    let edition_info_p = Param.mk ~description:["Edition info"] edition_info in 
+    let edition_p = Param.mk ~description:["Edition title"] Types.string in
+    let edition_info_p = Param.mk ~description:["Edition info"] edition_info in
     let current_params_p = Param.mk ~description:["Xapi paramaters"] stringPairLst in
     declare "apply_edition"
-    ["Checks license info and ensures provided features are compatible."]
-    ( debug_info_p @-> edition_p @-> current_params_p @-> returning edition_info_p err )
+      ["Checks license info and ensures provided features are compatible."]
+      ( debug_info_p @-> edition_p @-> current_params_p @-> returning edition_info_p err )
 
   let get_editions =
     let edition_list_p = Param.mk ~description:["List of editions"] edition_list in
     declare "get_editions"
-    ["Gets list of accepted editions."]
-    ( debug_info_p @-> returning edition_list_p err )
+      ["Gets list of accepted editions."]
+      ( debug_info_p @-> returning edition_list_p err )
 
   let get_version =
     let result_p = Param.mk ~description:["String of version."] Types.string in
     declare "get_version"
-    ["Returns version"]
-    ( debug_info_p @-> returning result_p err )
+      ["Returns version"]
+      ( debug_info_p @-> returning result_p err )
 
 end
